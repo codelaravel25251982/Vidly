43346e0767a4fc1875261ea2e596d310a57fd47c
DESKTOP-RT0VC3N\Tanakit <tkcode2525@gmail.com>
2017-07-07 06:52:43 +0700

    Learn All Result and url Parameter

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,6 +14,29 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() {Name = "Shrek!"};
             return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home",new { page = 1, sortby = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+
+            return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortby));
         }
     }
 }
